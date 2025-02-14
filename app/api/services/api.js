9a0bf4bb01a24615487afbdbad9af44bd2116b15
Hafiz Abubakar <hafiz@aquaproperties.com>
2025-02-14 11:29:26 +0400

    update api setting

--- app/api/services/api.js
+++ app/api/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://aquaproperties.com/api/load_properties";
 // const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 
